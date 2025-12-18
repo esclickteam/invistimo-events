6d68c6bfa2e3422ff3de7a57b4039a07fba27940
esclickteam <your-email@example.com>
2025-12-18 23:07:27 +0200

    הודעת קומית

--- app/components/seating/TableRenderer.jsx
+++ app/components/seating/TableRenderer.jsx
@@ -394,8 +394,7 @@ function TableRenderer({ table }) {
       {/* כסאות */}
       {seatsCoords.map((c, i) => {
         const guest = seatInfoMap.get(i)?.guest;
-        const rotation =
-          getSeatRotation(layout, c);
+        const rotation = getSeatRotation(layout, c);
 
         return (
           <Group key={i} x={c.x} y={c.y} rotation={rotation}>
