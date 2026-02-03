5aed753bafdc5400beae53832a3b2368b645a316
esclickteam <your-email@example.com>
2026-02-03 19:15:08 +0200

    הודעת קומית

--- app/[code]/route.ts
+++ app/[code]/route.ts
@@ -1,14 +1,17 @@
-import { NextResponse } from "next/server";
+import { NextRequest, NextResponse } from "next/server";
 import ShortLink from "@/models/ShortLink";
 import dbConnect from "@/lib/db";
 
 export async function GET(
-  req: Request,
-  { params }: { params: { code: string } }
+  request: NextRequest,
+  context: { params: Promise<{ code: string }> }
 ) {
   await dbConnect();
 
-  const link = await ShortLink.findOne({ code: params.code });
+  const { code } = await context.params;
+
+  const link = await ShortLink.findOne({ code });
+
   if (!link) {
     return NextResponse.redirect("https://invistimo.com/404");
   }
