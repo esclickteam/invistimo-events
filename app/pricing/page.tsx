8ba5fd47d6f02cfd0100f686780ab668f553f31c
esclickteam <your-email@example.com>
2026-02-14 20:05:44 +0200

    הודעת קומית

--- app/pricing/page.tsx
+++ app/pricing/page.tsx
@@ -296,7 +296,7 @@ export default function PricingPage() {
 <div className="text-center mb-5 flex flex-col items-center">
 
   <div className="absolute left-1/2 -translate-x-1/2 -top-[240px] md:-top-[300px] z-20 pointer-events-none">
-  <div className="relative w-[380px] h-[380px] md:w-[500px] md:h-[500px]">
+  <div className="relative w-[480px] h-[480px] md:w-[600px] md:h-[600px]">
     <Image
       src={planMeta[plan].icon}
       alt={planMeta[plan].alt}
